ca0cff922d49f7f0101eefa8d65426dd60a5b29a
Alex Merced <alex@alexmerced.dev>
2024-11-02 09:47:48 -0400

    update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,27 +3,23 @@ import { getCollection } from 'astro:content';
 
 export async function GET(context) {
   const blog = await getCollection('blog');
+
+  function generateSummary(content, length = 150) {
+    return content.length > length ? content.slice(0, length) + '...' : content;
+  }
+
   return rss({
-    // `<title>` field in output xml
-    title: 'The Data Lakehouse Blog',
-    // `<description>` field in output xml
-    description: 'A humble Astronaut’s guide to the stars',
-    // Pull in your project "site" from the endpoint context
-    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    title: 'AThe Data Lakehouse Hub',
+    description: 'Data Lakehouse Community and Content',
     site: context.site,
-    // Array of `<item>`s in output xml
-    // See "Generating items" section for examples using content collections and glob imports
     items: blog.map((post) => ({
       title: post.data.title,
-      pubDate: post.data.date,
-      description: post.data.description,
+      pubDate: post.data.pubDatetime,
+      description: generateSummary(post.body, 150),
       author: post.data.author,
       category: post.data.category,
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/blog/[slug]` routes
       link: `/blog/${post.slug}/`,
+      content: post.body,
     })),
-    // (optional) inject custom xml
-    customData: `<language>en-us</language>`,
   });
 }
